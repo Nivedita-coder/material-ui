93c8d533c65c57ff716eb5d6793319970da89f82
Nivedita-coder <niveditaprasad81@gmail.com>
2021-12-11 22:05:58 +0530

    feature02-a

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-import { AppBar } from '@mui/material';
+import { AppBar} from '@mui/material';
 import { Toolbar } from '@mui/material';
 import useScrollTrigger from '@mui/material/useScrollTrigger';
 import { makeStyles } from '@mui/styles';
+import Tabs from '@mui/material/Tabs';
+import Tab from "@mui/material/Tab";
+import  Button  from '@mui/material/Button';
+
 
 import logo from "../../assets/logo.svg";
+
 
 function ElevationScroll(props) {
     const { children } = props;
@@ -30,6 +35,23 @@ function ElevationScroll(props) {
       },
       logo:{
           height: "7em"
+      },
+      tabContainer:{
+          marginLeft: "auto"
+      },
+      tab: {
+        ...theme.typography.tab,  
+        minWidth: 10,
+        marginLeft: "25px"
+      },
+      button: {
+          ...theme.typography.estimate,
+          borderRadius: "50px",
+          marginLeft: "50px",
+          marginRight: "25px",
+          height: "45px",
+          
+
       }
   }))
   
@@ -46,6 +68,16 @@ export default function Header(props) {
             <AppBar  position="fixed">
                 <Toolbar disableGutters>
                     <img alt="company logo" className={classes.logo}src={logo} />
+                    <Tabs className={classes.tabContainer}>
+                        <Tab className={classes.tab} label="Home" />
+                        <Tab className={classes.tab} label="Services" />
+                        <Tab className={classes.tab} label="The Revolution" />
+                        <Tab className={classes.tab} label="About Us" />
+                        <Tab className={classes.tab} label="Contact Us" />
+                    </Tabs>
+                    <Button variant='contained' color="secondary" className={classes.button}>
+                        Free Estimate
+                    </Button>
                 </Toolbar>
             </AppBar>
         </ElevationScroll>
--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -1,8 +1,8 @@
 import { createTheme } from '@mui/material/styles'
 
 
-const arcBlue = "#0B72B9"
-const arcOrange=  "#b9560b"
+const arcBlue = "#0000D6"
+const arcOrange=  "#DD7A00"
 
 export default createTheme({
     palette:{
@@ -16,6 +16,21 @@ export default createTheme({
         },
         secondary: {
             main: `${arcOrange}`
+        }
+    },
+    typography:{
+        tab: {
+        fontFamily: "Raleway",
+        textTransform: "none",
+        fontWeight: 700,
+        fontSize: "1rem",
+        color: "#fffff"
+        },
+        estimate: {
+          fontFamily: "Pacifico",
+          fontSize: "1rem",
+          textTransform: "none",
+          color: "white"
         }
     }
 }); 
